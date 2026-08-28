6c14e4c06c39129bd396e8406b79427533c8c59b
uemmau <emmajoliegriffioen@gmail.com>
2026-08-28 12:08:07 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import BotonDog from "./componentes/BotonDog"
+import './App.css'
+import { useState } from "react"
+
+function App() {
+
+  const [isLogedIn, setIsLogedIn] = useState(false) 
+
+  function logUser(){
+
+    setIsLogedIn(!isLogedIn)
+  }
+
+  return (
+    <>
+    <h1>Registro</h1>
+    <h1>{isLogedIn ? "El usuario esta logueados" : "El usuario NO esta logeado"}</h1>
+    <button className = "log" onClick={logUser}> { isLogedIn ? "Cerrar sesion" : "Logearse"} </button>
+  
+    <h1> Perrot</h1> 
+
+      <BotonDog className="raza" nombreboton="Raza"/>
+
+      <BotonDog className="contacto"  nombreboton= "Contacto"/>
+
+      <BotonDog className="refugio" nombreboton= "Refugio"/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/BotonDog.jsx
@@ -0,0 +1,13 @@
+
+function BotonDog({nombreboton , className}){
+    return(
+        <>
+        <button className={className}>
+            {nombreboton}
+        </button>
+        </>
+    )
+
+}
+
+export default BotonDog;
